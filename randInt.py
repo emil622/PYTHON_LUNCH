b8c49653e6dce387ad12a3fe91326b12f068a274
E.M <emilutz09@gmail.com>
2012-03-21 22:34:49 +0200

    files added

---
+++ randInt.py
@@ -0,0 +1,22 @@
+
+
+
+import random
+
+__author__ = 'Emil'
+
+r = int( input('input range: ') )  #read the range of randomized numbers, you get a string, but later u need as integer
+n = int(input('number of outputs: ') )  #read the number of randimized numbers
+
+for count in range(0, n):
+    print (random.randint(1, r)) # this is yours, the one you need
+
+
+# 4 line of code, that's it.
+
+# put the code on GitHub, you need to go on the browser and create a repo there then follow the steps to initialize
+# and then do a clone on your pc. then add this code there and push/commit the file. then send me a link
+# with this file, so i can see the code. ok?
+#ok
+
+# i need to go now, bye
---
+++ randIntV2.py
@@ -0,0 +1,56 @@
+import sys
+import random
+import os
+'''
+__author__ = 'E.M.'
+
+n = int(sys.argv[1])
+range = int(sys.argv[2])  #read the range of randomized numbers, you get a string, but later u need as integer
+filename = sys.argv[3]     #reading the file name
+
+f = open(filename, 'a') # vezi modus... a=apend
+
+#print(n) # asa primesti parametri, intelegi?
+f.write('This is a random generated number list created with ' + sys.argv[0])
+'''
+
+'''
+for i in range(0,n):
+    #f.write(str(random.randint(1, range)))
+    print('test')
+
+for i in range(0,10):
+    f.write(random.randint(1, range))
+
+'''
+
+
+def my_help():
+    print(''' asdfsdf
+              sdfg
+              asdasdfsdf sda
+              asdfasdfasdf
+              s ''')
+
+#get user input
+print(sys.argv[0])
+n = int(sys.argv[1])
+m = int(sys.argv[2])
+filename = sys.argv[3]
+
+
+
+
+my_help()
+
+f = open(filename,'a') ## f.seek()
+
+# generate random numbers
+for i in range(0,n):
+    f.write('\n' + str(random.randint(0,m)))
+    print ('\t' + str(random.randint(0,m)))
+    f.write('\t' + str(random.uniform(1, m)))
+
+
+
+
