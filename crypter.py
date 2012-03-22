9a51317ce037cf00c4389a58b5671aff92cb764c
E.M <emilutz09@gmail.com>
2012-03-22 22:17:24 +0200

    commit test by E.M.

---
+++ crypter.py
@@ -0,0 +1,20 @@
+import sys
+
+__author__ = 'E.M.'
+#def lungime(chara):
+ #   if len(chara) < 7 :
+  #      while len(chara) < 7:
+   #         chara.insert(0,'0')
+
+key = sys.argv[1] #input('your key is: ')
+message = sys.argv[2] #input('your message is: ')
+k = 0
+crypt = ''
+for i in message:
+    crypt = crypt.append(key[k]^i)
+    if k == len(key)-1:
+        k = 0
+    else: k += 1
+print('your message is: ' + crypt + '     isn\'t?')
+
+
