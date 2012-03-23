6b44aa145ec67cfd393a231d159c5c52a110fd8d
E.M <emilutz09@gmail.com>
2012-03-23 17:31:17 +0200

    Temp_convert.py file committed by E.M.

---
+++ temp_convert.py
@@ -0,0 +1,13 @@
+import sys
+
+__author__ = 'E.M.'
+value = int(sys.argv[1])
+scale = sys.argv[2]
+if scale == 'Celsius':
+    temp = (9./5)*value+32
+    print ("%.1f" % temp)
+elif scale == 'Fahrenheit':
+    temp = (5./9)*(value-32)
+    print ("%.1f" % temp)
+    # -10 Fahrenheit => -23.3
+    # 22 Celsius => 71.6
