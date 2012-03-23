62a98a06c7f2a558dfe6ce51ee294e9ea55ab6e8
Katana20 <c.micu@me.com>
2012-03-23 20:49:28 +0100

    time added to the check_prime

--- check_prime.py
+++ check_prime.py
@@ -12,9 +12,16 @@
 #-------------------------------------------------------------------------------
 #P\Informatica\GITHUB\PYTHON_LUNCH
 import sys
+import time
 
-__author__ = 'E.M.'
-num = 7#int(sys.argv[1])
+start_time = time.time()
+
+
+num = 99999999 #int(sys.argv[1]) # 8 digits is the max num... how can you use greater numbers?
+               # Output
+               # 99999999 False
+               # (19.253000020980835, 'seconds')
+               # can u improve it to make it faster?
 validator = 1
 for i in range(2, int(num/2)):  #it checks if the modulus of number with al numbers within the range (2, number/2) is 0
     if num % i == 0:     #if it is 0 then the number is prime, else isn't
@@ -23,3 +30,5 @@ if validator == 1:
     print (repr(num) + ' True')
 else:
     print (repr(num) + ' False')
+
+print (time.time() - start_time, 'seconds')
