c059180b7d7f262c4a769011bfe7755cbadf3c40
E.M <emilutz09@gmail.com>
2012-03-23 20:46:09 +0200

    Temp_convert.py file committed by E.M.

--- check_prime.py
+++ check_prime.py
@@ -1,9 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        check_prime.py
+# Purpose:     Exercise
+#              Check if a given number is prime.
+#
+# Author:      E.M.
+#
+# Created:     23.03.2012
+# Copyright:   (c) emilutz09@gmail.com 2012
+# Version      1.0
+# Licence:     GPL
+#-------------------------------------------------------------------------------
+#P\Informatica\GITHUB\PYTHON_LUNCH
 import sys
 
 __author__ = 'E.M.'
-num = 121#int(sys.argv[1])
+num = 7#int(sys.argv[1])
 validator = 1
-for i in range(2, num/2):  #it checks if the modulus of number with al numbers within the range (2, number/2) is 0
+for i in range(2, int(num/2)):  #it checks if the modulus of number with al numbers within the range (2, number/2) is 0
     if num % i == 0:     #if it is 0 then the number is prime, else isn't
         validator = 0
 if validator == 1:
--- crypter.py
+++ crypter.py
@@ -5,16 +5,41 @@ __author__ = 'E.M.'
  #   if len(chara) < 7 :
   #      while len(chara) < 7:
    #         chara.insert(0,'0')
-
-key = sys.argv[1] #input('your key is: ')
+'''
+key = sys.argv[1] #input('your key is: ') # sorry, this is also ok
 message = sys.argv[2] #input('your message is: ')
+#message = 'dPTD@\SYRJ^G[S_JZM\@FZFWGLYUYTJKTWV'
+message="test"
 k = 0
 crypt = ''
+
 for i in message:
-    crypt = crypt.append(key[k]^i)
+
+    crypt = chr(ord("k")^ord(i))
+
     if k == len(key)-1:
         k = 0
-    else: k += 1
-print('your message is: ' + crypt + '     isn\'t?')
+    else:
+        k += 1
+
+    print (ord(crypt))
+'''
+from itertools import izip, cycle
+
+def xor_crypt_string(data, key):
+    return ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(data, cycle(key)))
+
+my_data = "mQU\\\x1a\x14S[\x14@[\x14GXQQD\x14Z[C\x15"
+my_key= "Python"
+
+# problma e k nu sunt aratate caracterele non-alpha numerice.
 
 
+
+# Do the actual encryption
+encrypted = xor_crypt_string(my_data, key=my_key)
+print repr(encrypted)
+
+# This will obtain the original data from the encrypted
+original = xor_crypt_string(encrypted, key=my_key)
+print repr(original)
