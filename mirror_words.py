867b515ce199b32a87acd4b1c4db3c4160acdd15
E.M <emilutz09@gmail.com>
2012-03-23 16:56:30 +0200

    commit test by E.M.

--- mirror_words.py
+++ mirror_words.py
@@ -2,4 +2,5 @@ import sys
 
 __author__ = 'E.M.'
 word=sys.argv[1]
-print(word[::-1])
+print(word[::-1]) #prints the sequence word (all of it specified by first ':')
+                    #and :-1 means that it starts from the end of it :)
