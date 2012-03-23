35924cd34fa907c29f3c46a9cd0796e82c0e031e
E.M <emilutz09@gmail.com>
2012-03-23 19:29:30 +0200

    Calculator.py file committed by E.M.

---
+++ calculator.py
@@ -0,0 +1,16 @@
+import sys
+
+__author__ = 'E.M.'
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[3])
+operator = '-'      #sys.argv[2]
+if operator == '+':
+    print (num1+num2)
+elif operator == '-':
+    print (num1-num2)
+elif operator == '/':
+    print (num1/num2)
+elif operator == '*':
+    print (num1*num2)
+else:
+    print ('wrong arguments!!')
